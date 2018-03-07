c1f68aa189268b48befeb859539ae0043d0036c9
Anurag Chowdhary <anc15791@gmail.com>
2018-03-07 03:03:44 -0800

    scripts

--- scripts/1_start_switch.sh
+++ scripts/1_start_switch.sh
@@ -1,1 +1,1 @@
-sudo ../../../../bmv2/targets/simple_switch/simple_switch -i 1@eth2 -i 2@eth3 --device-id 1 --log-console --thrift-port 9090 --nanolog ipc:///tmp/bm-1-log.ipc ../firewall_switch.json
+sudo ../../../../bmv2/targets/simple_switch/simple_switch -i 1@eth2 -i 2@eth3 --device-id 1 --thrift-port 9090 ../firewall_switch.json
