d73b584dafb3cd6ef89444e835c2fbc097d0d52f
Anurag Chowdhary <anc15791@gmail.com>
2018-03-08 14:03:43 -0800

    program

--- scripts/1_start_switch.sh
+++ scripts/1_start_switch.sh
@@ -1,1 +1,1 @@
-sudo ../../../../bmv2/targets/simple_switch/simple_switch -i 1@eth2 -i 2@eth3 --device-id 1 --thrift-port 9090 ../firewall_switch.json
+sudo ../../../../bmv2/targets/simple_switch/simple_switch -i 1@eth0 -i 2@eth1 --device-id 1 --thrift-port 9090 ../firewall_switch.json
